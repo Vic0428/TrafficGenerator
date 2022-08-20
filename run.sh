e6004a1061f0c779bc2e9fdea6b7810a2ac3dda2
Vic <wfeng@g.harvard.edu>
2022-08-20 10:42:17 -0400

    Tweak

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # Generate 10Gbps traffic based on TCP flow distribution (for 300ms)
-python generate.py --outfile traffic.dat --seed 0 --capacity 1e10 --load 0.9 --num_spines 1 --num_leaves 1 --num_servers 1 --end_time 0.3
+python generate.py --outfile traffic.dat --seed 0 --capacity 20e9 --load 1.0 --num_spines 1 --num_leaves 1 --num_servers 1 --end_time 0.3
